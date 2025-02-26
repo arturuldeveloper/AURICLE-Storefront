426f37b876af2da608b046ed4d0fa432d4fa32e0
bg-gitmm <info@brooksandgrant.com>
2025-02-26 16:42:27 +0000

    Fix dependency issues

--- gridsome.config.js
+++ gridsome.config.js
@@ -2,24 +2,26 @@ module.exports = {
   siteName: "Auricle Jewellery",
   chainWebpack(config) {
     config.plugin("html").tap((args) => {
-      args[0].meta = {
-        ...args[0].meta,
-        link: [
-          {
-            rel: "preconnect",
-            href: "https://fonts.googleapis.com",
-          },
-          {
-            rel: "preconnect",
-            href: "https://fonts.gstatic.com",
-            crossorigin: "anonymous",
-          },
-          {
-            rel: "stylesheet",
-            href: "https://fonts.googleapis.com/css2?family=YourFont:wght@400;700&display=swap",
-          },
-        ],
-      };
+      if (args[0]) {  // ✅ Ensure args[0] exists before modifying it
+        args[0].meta = {
+          ...args[0].meta,
+          link: [
+            {
+              rel: "preconnect",
+              href: "https://fonts.googleapis.com",
+            },
+            {
+              rel: "preconnect",
+              href: "https://fonts.gstatic.com",
+              crossorigin: "anonymous",
+            },
+            {
+              rel: "stylesheet",
+              href: "https://fonts.googleapis.com/css2?family=YourFont:wght@400;700&display=swap",
+            },
+          ],
+        };
+      }
       return args;
     });
   },
