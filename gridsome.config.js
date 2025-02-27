c9a08aef32d08eceef179c02ba832411663ec678
unknown <zdeveloper1231@gmail.com>
2025-02-28 01:43:23 +0200

    fixed build issue

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,26 +1,29 @@
 module.exports = {
   siteName: "Auricle Jewellery",
-  chainWebpack(config) {
-    config.plugin("html").tap((args) => {
-      if (!args[0]) return args; // 🛑 Prevents undefined error
-      args[0].meta = args[0].meta || {}; // 🛑 Ensures meta object exists
-
-      args[0].meta.link = [
-        {
-          rel: "preconnect",
-          href: "https://fonts.googleapis.com",
-        },
-        {
-          rel: "preconnect",
-          href: "https://fonts.gstatic.com",
-          crossorigin: "anonymous",
-        },
-        {
-          rel: "stylesheet",
-          href: "https://fonts.googleapis.com/css2?family=YourFont:wght@400;700&display=swap",
-        },
-      ];
-      return args;
-    });
+  vue: {
+    server: false, // Disable SSR if you don't need it
   },
+  // chainWebpack(config) {
+  //   config.plugin("html").tap((args) => {
+  //     if (!args[0]) return args; // 🛑 Prevents undefined error
+  //     args[0].meta = args[0].meta || {}; // 🛑 Ensures meta object exists
+
+  //     args[0].meta.link = [
+  //       {
+  //         rel: "preconnect",
+  //         href: "https://fonts.googleapis.com",
+  //       },
+  //       {
+  //         rel: "preconnect",
+  //         href: "https://fonts.gstatic.com",
+  //         crossorigin: "anonymous",
+  //       },
+  //       {
+  //         rel: "stylesheet",
+  //         href: "https://fonts.googleapis.com/css2?family=YourFont:wght@400;700&display=swap",
+  //       },
+  //     ];
+  //     return args;
+  //   });
+  // },
 };
